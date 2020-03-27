553ac22132d60b19bd25dd0a01bedd101337e574
SofiiaKlachko <62393261+SofiiaKlachko@users.noreply.github.com>
2020-03-27 11:20:54 +0200

    Create 2.cpp

---
+++ CW_1/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+float fun(float n){
+   return n;
+}
+int main(){
+int (*fcnPtr)() = fun;
+return 0;
+}
+int a,b;
+cout<<"input a="<<endl;
+cin>>a;
+cout<<"input b="<<endl;
+cin>>b;
+int main (void)
+{
+printf ("min of a, b is %d\n", min (a, b));
+return 0;
+}
