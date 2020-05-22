ea0d6aea7f228df4d73e4da5d9d63741f1a0c9f5
SofiiaKlachko <62393261+SofiiaKlachko@users.noreply.github.com>
2020-05-22 13:13:33 +0300

    Create 2.cpp

---
+++ CW_3(2)/2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+void main(){
+    ifstream studentA("in1.txt")
+    ifstream studentB("in2.txt")
+    ofstream output("out.txt")
+    string s,s2;
